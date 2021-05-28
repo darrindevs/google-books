001b769342718aa1d8a4b0634f1275d94a1ca5f2
Zaino <hello@darrin.dev>
2021-05-27 19:02:06 -0700

    update routes

---
+++ routes/api/books.js
@@ -0,0 +1,17 @@
+//✅
+const router = require("express").Router();
+const bookController = require("../../controllers/bookController");
+
+//  /api/books
+router.route("/")
+  .get(bookController.findAll)
+  .post(bookController.create);
+
+//  /api/books/:id
+router
+  .route("/:id")
+  .get(bookController.findById)
+  .put(bookController.update)
+  .delete(bookController.remove);
+
+module.exports = router;

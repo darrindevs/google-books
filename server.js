ce1c9da1effdf4ccef5702fe8205c1d82821669e
Zaino <hello@darrin.dev>
2021-05-27 19:41:16 -0700

    deploying

--- server.js
+++ server.js
@@ -23,6 +23,9 @@ if (process.env.NODE_ENV === "production") {
 // mongodb
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books",{
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
 });
 
 
